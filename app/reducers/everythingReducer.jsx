13ff782b30f7a40c4a9daec9370f616750fb3aa0
Jacquin Mininger <jacquin.mininger@gmail.com>
2017-02-28 17:42:59 -0500

    Almost Complete with the reducer--done for the night

--- app/reducers/everythingReducer.jsx
+++ app/reducers/everythingReducer.jsx
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+
+//ACTION DEFINITIONS
+//-----------------------------------------------------------------------------
+
 const POST_ITEM = 'POST_ITEM';
 const DELETE_ITEM = 'DELETE_ITEM';
 const DELETE_CART_ITEM = 'DELETE_CART_ITEM';
@@ -9,48 +13,63 @@ const ADD_CART_ITEM = 'ADD_CART_ITEM';
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
 
+
+//-----------------------------------------------------------------------------
+//INITIAL STATE
+
 const initialState = {
   itemsList: [],
   currentUser: {},
   cart: []
 };
 
+
+//-----------------------------------------------------------------------------
+// THE MAIN REDUCER THINGY!!!!!
+
 const rootReducer = (state = initialState, action) => {
 
   const newState = Object.assign({}, state);
 
   switch (action.type) {
+    
     case POST_ITEM:
-      newState.itemList = [...itemList, action.itemToPost];
-      //newState.itemList.push(action.itemToPost);
+      newState.itemList = [...state.cart, action.itemToPost];
       break;
-      //
-      // newState.itemsList = action.itemToPost;
-      // break;
 
     case DELETE_CART_ITEM:
-      // newState.cart
+      let newCart = [...state.cart];  //Makes duplicate cart array from initState
+      const index = newCart.indexOf(action.itemToDelete); //Finds the index of the element we want to delete
+      if (index){
+        delete newCart[index];       //Deletes element but leaves an undefined val in its place
+        newCart.filter(n => true); //ERIC DID THIS. IF IT DOESNT WORK...BLAME HIM
+      }
+      newState.cart = newCart;
+      break;
+
+    case DELETE_ITEM:
+      break;
+
+     case ADD_CART_ITEM:
+      let newCart = state.cart;
+      newCart.push(action.itemToAdd);
+      newState.cart = newCart;
+      break;
+
     default:
       return state;
   }
   return newState;
 };
 
-
+//-----------------------------------------------------------------------------
+//   ACTION CREATORS AND THUNK MIDDLEWARE
 
 //this is an action creator for post-item
-
 const postItemAction = (payload) => ({
   type: POST_ITEM,
   itemToPost: payload
 });
-
-const deleteCartItemAction = (payload) => (
-  store.dispatch({
-  type: DELETE_CART_ITEM,
-  itemToDelete: payload
-})
-);
 
 //create thunk action create
 const addItemToServer = () => {
@@ -62,8 +81,33 @@ const addItemToServer = () => {
  };
 };
 
+const deleteItem = (payload) => ({
+  type: DELETE_ITEM,
+  itemToDelete: payload
+});
+const deleteServerItem = () => {
+  return dispatch => {
+    axios.delete()
+  }
+}
 
-// const thunk
+
+//CREATES A DELETECARTITEM AND DISPATCHES
+const deleteCartItemAction = (payload) => ({
+    type: DELETE_CART_ITEM,
+    itemToDelete: payload
+  });
+//NOTE: NO CORRESPONDING THUNK BECAUSE THE CART IS CURRENTLY NON PERSISTENT
+
+
+const addCartItemAction = (payload) => ({
+    type: ADD_CART_ITEM,
+    itemToAdd: payload
+  });
+
+
+
+
 
 //Do we have to do this?
 // const loginAction = (payload) => ({
