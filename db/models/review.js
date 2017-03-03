bcd1d6cdee436dfa14886f466f054d3d810a985b
Joey Zheng <jzheng16@binghamton.edu>
2017-03-03 14:50:48 -0500

    feat(model):created review table

---
+++ db/models/review.js
@@ -0,0 +1,23 @@
+'use strict';
+const Sequelize = require('sequelize');
+const db = require('APP/db');
+
+
+//Get subject id by using a hook that looks at the item_id and queries the item data base to return the seller_id and then set that as the subject_id
+
+
+const Review = db.define('review', {
+  descripton: {
+    type: Sequelize.TEXT
+  },
+  rating: {
+    type: Sequelize.FLOAT
+  },
+
+  subject_id: {
+    type: Sequelize.INTEGER
+  }
+
+});
+
+module.exports = Review;
