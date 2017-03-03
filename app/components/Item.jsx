be33189fdf5b8e40c205d47599dcc6732a6d2962
Eric Yang <ericswyang@gmail.com>
2017-03-03 15:38:37 -0500

    feat(OneItemPage): added seller username

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default (props) => {
-
+  console.log(props);
   return (
     <div className="list-group-item col-md-6 col-md-offset-3">
       <h2>{props.item.title}</h2>
       <p>Price: ${props.item.price}</p>
       <p>Rating: {props.item.rating}/5</p>
       <p>Description: {props.item.description}</p>
-      <Link to={`/user/${props.item.seller_id}`} >By: CHANGE THIS TO THE USERNAME WITH EAGERLOADING</Link>
+      <Link to={`/user/${props.item.seller_id}`} >By: { props.item.seller && props.item.seller.name } </Link>
+
+
     </div>
   );
 };
