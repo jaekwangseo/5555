55f78c5bb1b0e51a1dc818ad2a818fbe5005f0ed
Jacquin Mininger <jacquin.mininger@gmail.com>
2017-03-06 18:12:39 -0500

    docs(added comments)

--- app/components/Payment.jsx
+++ app/components/Payment.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import axios from '../axios';
+import axios from 'axios';
 import {browserHistory} from 'react-router';
 
-const getPaymentInfo = function(synthE, orderId){
+
+//THIS FUNCTION GRABS INFO FROM THE PAYMENT FORM, POSTS IT TO THE API, THEN REROUTES TO EITHER
+//A SUCCESS OR ERROR PAGE
+//THE API ADDS TO THE PAYMENT DB, THEN UPDATES THE ORDER STATUS TO COMPLETE AND THE PAYMENT ID
+const getPaymentInfo = function(synthE, orderId){	//Does the synthE go first here? or the passed in arg?
 	const paymentObj = {
 		CCN: synthE.target.CCN,
     FirstName: synthE.target.fname,
@@ -27,7 +31,7 @@ export default function Payment(props){
 	return (
 		<div id="Payment">
 			<h3>Paymemt: </h3>
-				<form onSubmit={getPaymentInfo(props.cartId)}>
+				<form onSubmit={getPaymentInfo(props.cartId)}> {/*Can args be passed into OnClick Funcs like this?*/}
 					First name: <input type="text" name="fname" /><br />
 					Last name: <input type="text" name="lname" /><br />
 					Credit Card Number: <input type="number" name="CCN" width={16} /><br />
--- server/payment.js
+++ server/payment.js
@@ -1,17 +1,31 @@
 const db = require('APP/db');
 const Payment = db.model('payment');
 const Order = db.model('order');
+const OrderItem = db.model('order_item');
+const Item = db.model('items');
 
-const router = require('express').Router();// eslint-disable-line new-cap
-  router.post('/', (req, res, next) => {
-    Payment.create(req.body)
-    .then(newPayment => {
-			return Order.update({status: 'completed', payment_id: newPayment.id}, {where: {id: req.body.orderId}});// completed and paymentId
-    })
-    .then(order => {
-			res.json(order);
-    })
-    .catch(next);
-	});
+// const router = require('express').Router();// eslint-disable-line new-cap
+//   router.post('/', (req, res, next) => {
+//     let orderId = -1;
+//     Payment.create(req.body)
+//     .then(newPayment => {
+// 			return Order.update({status: 'completed', payment_id: newPayment.id}, {where: {id: req.body.orderId}});// completed and paymentId
+//     })
+//     .then(order => {
+// 			orderId = order.id;
+// 			return OrderItem.findAll({where: {order_id: order.id}});
+//     })
+//     .then(arrOfOrderItems => {
+//     	Promise.map(arrOfOrderItems, function(orderItem){
+//     		 Item.findOne({where: {id: orderItem.item_id}})
+//     		.then(item => {
+//     			return OrderItem;
+//     		})
+//     	})
+			
+//     })
+//     .catch(next);
+// 	});
 
-  module.exports = router;
+//   module.exports = router;
+// res.json(orderId);
