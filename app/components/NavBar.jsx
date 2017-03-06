dfc35bb00910e0fd1f9253abc6c32ac7b994c982
Eric Yang <ericswyang@gmail.com>
2017-03-06 12:40:39 -0500

    feat(signup navbar)

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,24 +1,8 @@
 //this navbar was copy pasted from bootstrap websites.
 
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { logout } from '../reducers/auth.jsx';
 
-
-// class NavBarContainer extends React.Component {
-//
-//   constructor(props){
-//     super(props);
-//   }
-//
-//   onLogoutClick (event) {
-//     this.props.authNull({
-//
-//     })
-//   }
-//
-// }
 export default (props) =>
 {
   console.log(props);
@@ -40,14 +24,14 @@ export default (props) =>
 					<li>
 						<Link to='/' >Home </Link>
 					</li>
-					<li>
-            <Link to={ props.auth && `/user/${props.auth.id}`}>My Account</Link>
-					</li>
 				</ul>
 				<ul className="nav navbar-nav navbar-right">
 					<li>
 						<Link to='/cart' >Shopping Cart </Link>
 					</li>
+          <li>
+            { props.auth ? <Link to={`/user/${props.auth.id}`}>My Account</Link> : <Link to='/createUser'>Sign Up</Link>}
+          </li>
 					<li>
 						{props.auth ? <Link to='/home' onClick={ () => props.handleLogoutClick() }>Logout</Link> : <Link to='/login' >Login</Link>}
 					</li>
