bfd7a6faa486f7cb11579f6296272a1bfb1d114b
Eric Yang <ericswyang@gmail.com>
2017-03-06 17:31:51 -0500

    feat(userURL button): its a linkedin icon, can change... this commit has lots of unfinished business for filters and add to cart buttons

--- app/components/Items.jsx
+++ app/components/Items.jsx
@@ -3,13 +3,39 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+
 export default function Items(props) {
 
+  const catOptionNames = [
+    { value: 'one', label: 'javascript' },
+    { value: 'two', label: 'joey', clearableValue: false }
+];
 
-  console.log('rendering items', props);
+  console.log('rendering items', props.categories);
   return (
     <div>
-      <ul className="list-group">
+      <div className="section">
+        <h3 className="section-heading">Multiselect</h3>
+        <div className="Select Select--multi is-searchable">
+          <div className="Select-control">
+            <span className="Select-multi-value-wrapper" id="react-select-3--value">
+              <div className="Select-placeholder">Select your favourite(s)</div>
+              <div className="Select-input" style={{display: "inline-block" }}>
+                <input role="combobox" aria-expanded="false" aria-owns="" aria-haspopup="false" aria-activedescendant="react-select-3--value" value="" style={{width: "500px"}} />
+
+
+              </div>
+              </span>
+              <span className="Select-arrow-zone">
+                <span className="Select-arrow">
+                </span>
+                </span>
+            </div>
+          </div>
+          <div className="checkbox-list"><label className="checkbox"><input type="checkbox" className="checkbox-control" value="on" /><span className="checkbox-label">Disable the control</span></label><label className="checkbox"><input type="checkbox" className="checkbox-control" value="on" /><span className="checkbox-label">I don't like Chocolate (disabled the option)
+          </span></label></div></div>
+
+        <ul className="list-group">
         {
           props.itemList && props.itemList.map( (item) => (
             <li key={item.id} className="list-group-item col-md-6 col-md-offset-3">
@@ -19,7 +45,9 @@ export default function Items(props) {
               <div className="list-group-item-text">
                 {item.description}
               </div>
-              <button className="add-to-cart" onClick={(event) => {event.preventDefault(); props.addItemToCart(item.id);}} >Add to cart</button>
+              <div className="container button-wrapper">
+	               <button className="btn-lg btn-success col-md-offset-3" onClick={(event) => {event.preventDefault(); props.addItemToCart(item.id);}}>Add <span className="glyphicon glyphicon-shopping-cart"></span></button>
+              </div>
               {props.user && props.user.admin ?
                 <div>
                   <button onClick={() => props.handleDeleteUser(item.id)} >
@@ -42,25 +70,30 @@ export default function Items(props) {
           Add Item
       </button>
 
-      <form className="review" onSubmit={props.handleFilterEvent}>
 
-
-          <div className="form-group col-md-offset-1 col-md-3">
-            <label htmlFor="category">Filter by Category</label> <br />
-            <input type="radio" name="category" value="Javascript" />  1
-            <select name="category">
-              {props.categories.map(category => (
-                <option key={category.id} value={category.name}>{category.name}</option>
-
-                ))}
-
-            </select>
-
-          </div>
-
-          <button type="submit" className="btn btn-success col-md-2 col-md-offset-5" >Submit</button>
-      </form>
     </div>
 
   );
 }
+
+
+/*
+<form className="review" onSubmit={props.handleFilterEvent}>
+
+
+    <div className="form-group col-md-offset-1 col-md-3">
+      <label htmlFor="category">Filter by Category</label> <br />
+      <input type="radio" name="category" value="Javascript" />  1
+      <select name="category">
+        {props.categories.map(category => (
+          <option key={category.id} value={category.name}>{category.name}</option>
+
+          ))}
+
+      </select>
+
+    </div>
+
+    <button type="submit" className="btn btn-success col-md-2 col-md-offset-5" >Submit</button>
+</form>
+*/
